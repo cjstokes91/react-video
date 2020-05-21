f3d80b85d48d12be3ec49c429ff17b98affaa54e
carl stokes <carlstokes@carls-MBP.lan>
2020-05-21 15:01:31 -0700

    youtube api

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,30 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+    state = { searchTerm: '' };
+
+    onIputChange = (event) => {
+        this.setState({ searchTerm: event.target.value })
+
+    };
+
+    onFormSubmit = event => {
+        event.preventDefault();
+        // todo : make sure we call 
+        // call back from parent
+    };
+
     render() {
         return (
             <div className='search-bar ui segment'>
-                <form className='ui form'>
+                <form onSubmit={this.onFormSubmit} className='ui form'>
                     <div className='field'>
                         <label>Video Search</label>
-                        <input type='text' />
+                        <input type='text'
+                            value={this.state.searchTerm}
+                            onChange={this.onIputChange}
+                        />
+
                     </div>
                 </form>
             </div>
