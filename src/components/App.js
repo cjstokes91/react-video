f0710c4d5d9d4dfb70234cae712552f24d9837c5
carl stokes <carlstokes@carls-MBP.lan>
2020-05-25 15:55:15 -0700

    style to body

--- src/components/App.js
+++ src/components/App.js
@@ -3,21 +3,30 @@ import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 import VideoList from './VideoList';
 import VideoDetail from './VideoDetail';
-
+import './App.css';
 class App extends React.Component {
     state = { videos: [], selectedVideo: null };
+
+    componentDidMount = () => {
+        this.onTermSubmit('YG')
+    }
+
     onTermSubmit = async searchTerm => {
         const response = await youtube.get('./search', {
             params: {
                 q: searchTerm
             }
         });
-        this.setState({ videos: response.data.items })
+        this.setState({
+            videos: response.data.items,
+            selectedVideo: response.data.items[0]
+        })
     }
 
     onVideoSelect = (video) => {
         this.setState({ selectedVideo: video })
     }
+
 
 
     render() {
